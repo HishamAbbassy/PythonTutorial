86fa9a2f56875830fcfe95826d02fa43e7bfcef9
Hisham Abbassy <hisham.abbassy@gmail.com>
2026-01-20 17:59:11 +0100

    User asked at the beginning how many times he wants to toss the coin

--- Automate_The_Boring_Stuff/Basics/Loops/CoinTossN.py
+++ Automate_The_Boring_Stuff/Basics/Loops/CoinTossN.py
@@ -1,13 +1,23 @@
 import random
 
 print(' Let us play Head or Tail')
+print('How many times you want to play? ')
+
+N = input('>')
+N = int(N)
+if N > 0:
+
+    print('(H)ead or (T)ail?')
+    S = input('>')
+else:
+    print('Good Bye')
 
 def Head_Tail(n):
 
     h = 0
     t = 0
 
-    for i in range(n):  # Perform n coin flips.
+    for i in range(n):  # Perform 100 coin flips.
 
         if random.randint(0, 1) == 0:
             print('H', end=' ')
@@ -18,9 +28,31 @@ def Head_Tail(n):
             t = t + 1
 
     print('Head: ' + str(h) + ' ,Tail: ' + str(t))  # Print one newline at the end.
- 
+    return h, t
 
-Head_Tail (10)
-Head_Tail (100)
-Head_Tail(10000)
+#Head_Tail (10)
+#Head_Tail (100)
+#Head_Tail(10000)
 
+if S == 'H':
+    h,t = Head_Tail (N)
+   
+    if h > t:
+        print('You WON!')
+    elif h == t:
+        print('Draw')
+    else:
+        print('you Lost')
+
+elif S == 'T':
+    h,t = Head_Tail (N)
+   
+    if t > h:
+        print('You WON!')
+    elif h == t:
+        print('Draw')
+    else:
+        print('you Lost')
+
+else:
+    print('Wrong Choice')
